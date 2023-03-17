6baef3d5f24ff2b14d748afcf1ba896b7412a07d
sieman216485 <119700542+sieman216485@users.noreply.github.com>
2023-03-17 11:09:37 -0400

    perf: add OpenCV tracker class

--- GOTURN_Tracker_OpenCV_Python/main_rect.py
+++ GOTURN_Tracker_OpenCV_Python/main_rect.py
@@ -1,88 +1,9 @@
 import cv2
 import sys
 import time
-
-def compare_opencv_version(major, minor, subminor):
-    (current_major, current_minor, current_subminor) = cv2.__version__.split(".")
-
-    current_major = int(current_major)
-    current_minor = int(current_minor)
-    current_subminor = int(current_subminor)
-
-    if major >= 0:
-        if major > current_major:
-            return -1
-        elif major < current_major:
-            return 1
-
-    if minor >= 0:
-        if minor > current_minor:
-            return -1
-        elif minor < current_minor:
-            return 1
-
-    if subminor >= 0:
-        if subminor > current_subminor:
-            return -1
-        elif subminor < current_subminor:
-            return 1
-
-    return 0
+from tracker import *
 
 if __name__ == "__main__":
-
-    # OpenCV tracker types
-    TRACKER_BOOSTING = "BOOSTING"
-    TRACKER_MIL = "MIL"
-    TRACKER_KCF = "KCF"
-    TRACKER_TLD = "TLD"
-    TRACKER_MEDIANFLOW = "MEDIANFLOW"
-    TRACKER_GOTURN = "GOTURN"
-    TRACKER_MOSSE = "MOSSE"
-    TRACKER_CSRT = "CSRT"
-
-    # tracker_type = TRACKER_GOTURN
-    tracker_type = TRACKER_KCF
-
-    # if int(minor_ver) < 3:
-    if compare_opencv_version(-1, 3, -1) < 0:
-        tracker = cv2.Tracker_create(tracker_type)
-    else:
-        if tracker_type == TRACKER_BOOSTING:
-            if compare_opencv_version(4, 5, 1) < 0:
-                tracker = cv2.TrackerBoosting_create()
-            else:
-                tracker = cv2.legacy.upgradeTrackingAPI(cv2.legacy.TrackerBoosting_create())
-
-        if tracker_type == TRACKER_MIL:
-            tracker = cv2.TrackerMIL_create()
-
-        if tracker_type == TRACKER_KCF:
-            tracker = cv2.TrackerKCF_create()
-
-        if tracker_type == TRACKER_TLD:
-            if compare_opencv_version(4, 5, 1) < 0:
-                tracker = cv2.TrackerTLD_create()
-            else:
-                tracker = cv2.legacy.upgradeTrackingAPI(cv2.legacy.TrackerTLD_create())
-
-        if tracker_type == TRACKER_MEDIANFLOW:
-            if compare_opencv_version(4, 5, 1) < 0:
-                tracker = cv2.TrackerMedianFlow_create()
-            else:
-                tracker = cv2.legacy.upgradeTrackingAPI(cv2.legacy.TrackerMedianFlow_create())
-
-        if tracker_type == TRACKER_GOTURN:
-            tracker = cv2.TrackerGOTURN_create()
-
-        if tracker_type == TRACKER_MOSSE:
-            if compare_opencv_version(4, 5, 1) < 0:
-                tracker = cv2.TrackerMOSSE_create()
-            else:
-                tracker = cv2.legacy.upgradeTrackingAPI(cv2.legacy.TrackerMOSSE_create())
-
-        if tracker_type == TRACKER_CSRT:
-            tracker = cv2.TrackerCSRT_create()
 
     # Open video file
     capture = cv2.VideoCapture("../Test_Video_Files/cars.mp4")
@@ -107,7 +28,8 @@ if __name__ == "__main__":
     # Select a bounding box
     bounding_box = cv2.selectROI(frame, False)
 
-    # Initialize tracker with first frame and bounding box
+    # Create a OpenCV tracker and Initialize tracker with first frame and bounding box
+    tracker = OpenCVTracker()
     ok = tracker.init(frame, bounding_box)
 
     while True:
@@ -140,7 +62,7 @@ if __name__ == "__main__":
             start = time.time_ns()
 
         # Display tracker type on frame
-        cv2.putText(frame, tracker_type + " Tracker", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
+        cv2.putText(frame, tracker.type(), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2)
 
         # Display FPS on frame
         if fps > 0:
